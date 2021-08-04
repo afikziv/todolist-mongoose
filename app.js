f5f573e4ae5c93e3fbce5d4ce178f536733d9968
afik <afikziv@gmail.com>
2021-08-04 15:18:59 +0300

    initial commit

--- app.js
+++ app.js
@@ -139,8 +139,9 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
 }
-app.listen(port,function(){
-  console.log("server started Successfully");
+
+app.listen(port, function() {
+  console.log("Server started succesfully");
 });
 
 app.listen(3000, function() {
